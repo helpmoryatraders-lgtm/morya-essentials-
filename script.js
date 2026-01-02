6a06b4c8963ef14fd658f05c2af063f5f8568f76
helpmoryatraders-lgtm <help.moryatraders@gmail.com>
2026-01-02 18:28:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+// Festival auto change
 const festivals = [
   "Diwali Festival 🪔",
   "Ganesh Chaturthi 🐘",
-  "Navratri 🌸",
+  "Navratri 🌺",
   "Daily Puja Essentials 🙏"
 ];
 
-let i = 0;
-setInterval(() => {
-  const el = document.getElementById("festivalText");
-  if (el) {
-    el.innerText = festivals[i];
-    i = (i + 1) % festivals.length;
+let i=0;
+setInterval(()=>{
+  const el=document.getElementById("festivalText");
+  if(el){
+    el.innerText=festivals[i];
+    i=(i+1)%festivals.length;
   }
-}, 2500);
+},2500);
+
+// Menu toggle
+function toggleMenu(){
+  document.getElementById("nav").classList.toggle("active");
+}
